441ffdc05f5812cde4478657b8c79387d4a50f98
Jordi Prats <jprats@systemadmin.es>
2017-07-19 16:34:59 +0200

    template

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+class tinyproxy (
+                  $manage_package        = true,
+                  $package_ensure        = 'installed',
+                  $manage_service        = true,
+                  $manage_docker_service = true,
+                  $service_ensure        = 'running',
+                  $service_enable        = true,
+                ) inherits tinyproxy::params{
+
+  validate_re($package_ensure, [ '^present$', '^installed$', '^absent$', '^purged$', '^held$', '^latest$' ], 'Not a supported package_ensure: present/absent/purged/held/latest')
+
+  class { '::tinyproxy::install': }
+  -> class { '::tinyproxy::config': }
+  ~> class { '::tinyproxy::service': }
+  -> Class['::tinyproxy']
+
+}
